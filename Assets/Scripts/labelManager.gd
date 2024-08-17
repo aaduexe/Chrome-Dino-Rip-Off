79ca4df5ea67ed060bcadc61629fc5f1119e3b48
Taledrawer <randomwebsiteandapp@gmail.com>
2024-08-17 16:49:43 +0530

    Distance label implemented with 2 decimal place

--- Assets/Scripts/labelManager.gd
+++ Assets/Scripts/labelManager.gd
@@ -6,7 +6,9 @@ extends Label
 
 func _process(delta):
 	var distance_display_int = int(GameManager.distance)
-	var sin_distance = roundf(distance_display_int / 1000)
+	var KM_distance = GameManager.distance / 1000
+	var sin_distance = "%0.2f" % KM_distance
+	
 	if distance_display_int < 1000:
 		label_score.text = "Distance: " + str(distance_display_int) + "m"
 	elif distance_display_int >= 1000:
